70dc75042ff709270b07276b7a9e187ec0f5a5cc
Brenn Santiago <brennaldwin.santiago@gmail.com>
2024-10-29 16:30:52 +0800

    create van detail page

--- src/pages/vans/VanDetail.tsx
+++ src/pages/vans/VanDetail.tsx
@@ -1,12 +1,52 @@
-
+import { Link, useParams } from "react-router-dom";
 import VanLayout from "./VanLayout";
+import { useEffect, useState } from "react";
+import { Van } from "../../types/vans";
 
 const VanDetail = () => {
+
+  const params = useParams()
+  console.log(params)
+
+  const [van, setVan] = useState<Van>()
+
+  useEffect(() => {
+    fetch(`/api/vans/${params.id}`)
+    .then(res => res.json())
+    .then(data => setVan(data.vans))
+  }, [params])
 
   return ( 
     <VanLayout>
       <div>
-        Van details goes here..
+        <span>←</span>
+        <span className="ml-1 underline text-sm text-gray-600">
+          <Link to="/vans">
+            Back to all vans
+          </Link>
+        </span>
+        {van ? (
+          <div className="flex gap-10 flex-col sm:flex-row">
+            <img src={van.imageUrl} className="rounded-lg h-auto sm:h-96"/>
+            <div className="flex flex-col gap-5 items-start">
+              <button 
+                className={`
+                    text-orange-50 px-3 py-1 rounded-md
+                    ${van.type === "Simple" && "bg-red-500"}
+                    ${van.type === "Rugged" && "bg-green-900"}
+                    ${van.type === "Luxury" && "bg-slate-950"}
+                  `}
+                >
+                  {van.type}
+                </button>
+                <div className="flex flex-col text-2xl font-bold">
+                  <h1>{van.name}</h1>
+                  <p>${van.price}<span className="text-sm text-gray-500">/day</span></p>
+                </div>
+                <p>{van.description}</p>
+            </div>
+          </div>
+        ) : <h2> Loading... </h2>}
       </div>
     </VanLayout>
 
