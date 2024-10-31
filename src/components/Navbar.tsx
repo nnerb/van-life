9219b3eca7771e8d875a63ef87200742de73d254
Brenn Santiago <brennaldwin.santiago@gmail.com>
2024-10-31 16:08:19 +0800

    modify navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,8 +8,8 @@ const routes = [
 
 const Navbar = () => {
   return ( 
-    <div className="fixed top-0 bg-orange-50 w-full py-10 z-10 shadow-md">
-      <div className="max-w-screen-lg mx-auto flex items-center w-full px-8">
+    <div className="fixed top-0 bg-orange-50 w-full py-10 z-10 shadow-md ">
+      <div className="max-w-screen-lg mx-auto flex items-center w-full px-8 gap-12">
         <Link  to="/" className="font-inter-black text-3xl">#VANLIFE</Link>
         <div className="ml-auto text-base flex gap-3 font-inter-semi-bold">
           {routes.map((route) => (
