752659812afe4baebc426ea33dd43c7ccead7abb
Brenn Santiago <brennaldwin.santiago@gmail.com>
2024-12-01 23:40:59 +0800

    modify offline error component add margin top

--- src/components/OfflineError.tsx
+++ src/components/OfflineError.tsx
@@ -1,6 +1,6 @@
 const OfflineError = () => {
   return (
-    <p className="text-center text-xl font-semibold text-red-600 bg-red-100 p-4 rounded-md shadow-md">
+    <p className="text-center text-xl font-semibold text-red-600 bg-red-100 p-4 rounded-md shadow-md mt-5">
       No network connection. Please check your internet connection.
     </p>
   );
